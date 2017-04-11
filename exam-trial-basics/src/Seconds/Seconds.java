9330f6f236edfacdb5c994bd5b9d64d1972f835d
Kristof Majoros <akmajoros1@gmail.com>
2017-04-11 15:37:04 +0200

    finished Seconds exercise

--- exam-trial-basics/src/Seconds/Seconds.java
+++ exam-trial-basics/src/Seconds/Seconds.java
@@ -1,8 +1,21 @@
 package Seconds;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Seconds {
+   public void listDivider(){
+    List<Object> toPrint = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    for (int i = 0; i < toPrint.size(); i++) {
+      if (i % 2 == 1) {
+        System.out.print(toPrint.get(i) + " ");
+      }
+    }
+  }
 
+  public static void main(String[] args) {
+     Seconds seconds = new Seconds();
+    seconds.listDivider();
+  }
 }
