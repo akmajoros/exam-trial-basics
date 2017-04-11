0f369ac2a4dd57194d203f2739703a01e897eff7
Kristof Majoros <akmajoros1@gmail.com>
2017-04-11 17:01:41 +0200

    pirateCounter function prints the correct names but has no return yet

--- exam-trial-basics/src/Pirates/PirateCounter.java
+++ exam-trial-basics/src/Pirates/PirateCounter.java
@@ -3,17 +3,25 @@ import java.util.*;
 
 public class PirateCounter{
 
-  public static void main(String... args){
-    ArrayList<Pirate> pirates = new ArrayList<>();
-    pirates.add(new Pirate("Olaf", false, 12);
-    pirates.add(new Pirate("Uwe", true, 9);
-    pirates.add(new Pirate("Jack", true, 16);
-    pirates.add(new Pirate("Morgan", false, 17);
-    pirates.add(new Pirate("Hook", true, 20);
+  public void pirateCount (ArrayList arrayList) {
+    String pirateCounter;
+    for (int i = 0; i < arrayList.size(); i++) {
+      Pirate pirate = (Pirate) arrayList.get(i);
+      if (pirate.hasWoodenLeg && pirate.gold > 15) {
+        pirateCounter = pirate.name;
+        System.out.println(pirateCounter);
+      }
+    }
+  }
 
-    // Write a function that takes any list that contains pirates as in the example,
-    // And returns a list of names containing the pirates that
-    // - have wooden leg and
-    // - have more than 15 gold
+  public static void main(String... args){
+    PirateCounter pirateCounter = new PirateCounter();
+    ArrayList<Pirate> pirates = new ArrayList<>();
+    pirates.add(new Pirate("Olaf", false, 12));
+    pirates.add(new Pirate("Uwe", true, 9));
+    pirates.add(new Pirate("Jack", true, 16));
+    pirates.add(new Pirate("Morgan", false, 17));
+    pirates.add(new Pirate("Hook", true, 20));
+    pirateCounter.pirateCount(pirates);
   }
 }
