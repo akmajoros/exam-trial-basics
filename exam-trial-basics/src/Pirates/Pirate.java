6ced2e5b1e02a20b16eb2deadc2d7fa7cfd08480
Kristof Majoros <akmajoros1@gmail.com>
2017-04-11 16:43:35 +0200

    Pirates initial commit

---
+++ exam-trial-basics/src/Pirates/Pirate.java
@@ -0,0 +1,15 @@
+package Pirates;
+
+public class Pirate {
+    String name;
+    boolean hasWoodenLeg;
+    int gold;
+
+    Pirate(String name, boolean hasWoodenLeg, int gold) {
+      this.name = name;
+      this.hasWoodenLeg = hasWoodenLeg;
+      this.gold = gold;
+    }
+  }
+
+  
